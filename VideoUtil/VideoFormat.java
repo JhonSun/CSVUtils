00c0dd348a874cde5a7a6d13551483abf2f71131
JhonSun <1193086345@qq.com>
2019-07-23 16:53:19 +0800

    修改工具类
    之前在linux服务器上无法使用，修改为通用版

--- VideoUtil/VideoFormat.java
+++ VideoUtil/VideoFormat.java
@@ -5,36 +5,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.ruoyi.common.utils.StringUtils;
+package com.ruoyi.common.utils.video;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class VideoFormat {
 	
+	public static final Logger log = LoggerFactory.getLogger(VideoFormat.class);
 	// ffmpeg.exe 默认路径
-	private static final String FFMPEG_PATH = "\\ruoyi-common\\src\\main\\java\\com\\ruoyi\\common\\utils\\video\\";
+//	private static final String FFMPEG_PATH = "/home/ffmpeg/bin/./ffmpeg";
+	private static final String FFMPEG_PATH = "E:\\program\\cram\\lib\\ffmpeg.exe";
 
 	public static void main(String[] args) {
-		String currPath = getPath();
 
-		if (!checkfile("E:\\test\\start\\6.rm")) {
-			System.out.println("E:\\test\\start\\6.rm" + " is not file");
+		if (!checkfile("E:\\test\\start\\11.rmvb")) {
+			System.out.println("E:\\test\\start\\11.rmvb" + " is not file");
 			return;
 		}
-		if (process("E:\\test\\start\\6.rm","E:\\test\\end\\",currPath+"\\src\\main\\java\\com\\ruoyi\\common\\utils\\video\\")) {
+		if (process("E:\\test\\start\\11.rmvb","E:\\test\\start\\")) {
 			System.out.println("ok");
 		}
 	}
 	
-	public static boolean videoFormat(String inputPath,String outputPath,String ffmpegPath) {
-		if (StringUtils.isBlank(ffmpegPath)) {
-			ffmpegPath = FFMPEG_PATH;
-		}
+	public static boolean videoFormat(String inputPath,String outputPath) {
 		
-		String currPath = getPath();
-
 		if (!checkfile(inputPath)) {
+			log.info("checkfile不通过");
 			System.out.println(inputPath+" is not file");
 			return false;
 		}
-		if (process(inputPath,outputPath,currPath+ffmpegPath)) {
+		log.info("视频上传转MP4开始插件路径："+ FFMPEG_PATH);
+		if (process(inputPath,outputPath)) {
 			System.out.println("ok");
 			return true;
 		}else {
@@ -42,47 +52,28 @@ public class VideoFormat {
 		}
 	}
 
-	public static String getPath() {
-		// 先获取当前项目路径，在获得源文件、目标文件、转换器的路径
-		File diretory = new File("");
-		try {
-			String currPath = diretory.getAbsolutePath();
-			currPath = currPath.substring(0, currPath.lastIndexOf("\\"));
-//			inputPath = "E:\\test\\start\\6.rm";//需要转换格式的视频文件路径
-//			outputPath = "E:\\test\\end\\";//转换后的MP4文件路径
-//			ffmpegPath = "E:\\test\\min\\";//ffmpeg插件路径
-			System.out.println(currPath+" 1111111is not file");
-			return currPath;
-		} catch (Exception e) {
-			System.out.println("getPath出错");
-			return null;
-		}
-	}
-
-	public static boolean process(String inputPath,String outputPath,String ffmpegPath) {
+	public static boolean process(String inputPath,String outputPath) {
 		// int type = checkContentType();
 		boolean status = false;
 		System.out.println("直接转成mp4格式");
-		status = processMp4(inputPath,outputPath,ffmpegPath);// 直接转成mp4格式
+		log.info("直接转成mp4格式"+inputPath+"----"+outputPath+"----"+FFMPEG_PATH);
+		status = processMp4(inputPath,outputPath);// 直接转成mp4格式
 		return status;
 	}
 
 	private static boolean checkfile(String path) {
 		File file = new File(path);
+		log.info("file===="+file);
 		if (!file.isFile()) {
 			return false;
 		}
 		return true;
 	}
 
-	private static boolean processMp4(String inputPath,String outputPath,String ffmpegPath) {
-
-		if (!checkfile(inputPath)) {
-			System.out.println(inputPath + " is not file");
-			return false;
-		}
+	private static boolean processMp4(String inputPath,String outputPath) {
+		log.info("进入转换");
 		List<String> command = new ArrayList<String>();
-		command.add(ffmpegPath + "ffmpeg.exe");
+		command.add(FFMPEG_PATH);
 		command.add("-i");
 		command.add(inputPath);
 		command.add("-c:v");
@@ -98,62 +89,31 @@ public class VideoFormat {
 		command.add("-movflags");
 		command.add("faststart");
 		command.add(outputPath + ".mp4");
-		try {
-
-			// 方案1
-			// Process videoProcess = Runtime.getRuntime().exec(ffmpegPath + "ffmpeg -i " +
-			// oldfilepath
-			// + " -ab 56 -ar 22050 -qscale 8 -r 15 -s 600x500 "
-			// + outputPath + "a.flv");
-
-			// 方案2
-			Process videoProcess = new ProcessBuilder(command).redirectErrorStream(true).start();
-
-			new PrintStream(videoProcess.getErrorStream()).start();
-
-			new PrintStream(videoProcess.getInputStream()).start();
-
-			videoProcess.waitFor();
-
-			return true;
-		} catch (Exception e) {
+	     StringBuffer test=new StringBuffer();    
+	     for(int i=0;i<command.size();i++) {
+	    	 test.append(command.get(i)+" "); 
+	     }    
+	     log.info("视频转换test"+test);
+	    try {
+			  Runtime rt = Runtime.getRuntime();
+                          Process proc = rt.exec(test.toString());
+			  InputStream stderr = proc.getErrorStream();
+			  InputStreamReader isr = new InputStreamReader(stderr);
+			  BufferedReader br = new BufferedReader(isr);
+			  String line = null;
+			  while ( (line = br.readLine()) != null) {
+			        System.out.println(line);
+			  };
+		
+		} catch (IOException e) {
 			e.printStackTrace();
 			return false;
 		}
+		log.info("视频转换成功");
+		return true;
 	}
 
-	// private static int checkContentType() {
-	// String type = inputPath.substring(inputPath.lastIndexOf(".") + 1,
-	// inputPath.length()).toLowerCase();
-	// // ffmpeg能解析的格式：（asx，asf，mpg，wmv，3gp，mp4，mov，avi，flv等）
-	// if (type.equals("avi")) { // ok
-	// return 0;
-	// } else if (type.equals("mpg")) {// ok
-	// return 0;
-	// } else if (type.equals("wmv")) {// ok
-	// return 0;
-	// } else if (type.equals("3gp")) { // ok
-	// return 0;
-	// } else if (type.equals("mov")) { // ok
-	// return 0;
-	// } else if (type.equals("mp4")) {
-	// return 0;
-	// } else if (type.equals("flv")) { // ok
-	// return 0;
-	// } else if (type.equals("mkv")) { // ok
-	// return 0;
-	// } else if (type.equals("vob")) { // ok
-	// return 0;
-	// } else if (type.equals("swf")) { // ok
-	// return 0;
-	// } else if (type.equals("gif")) { // ok
-	// return 0;
-	// } else if (type.equals("rm")) { // ok
-	// return 0;
-	// } else if (type.equals("rmvb")) { // ok
-	// return 0;
-	// }
-	// return 9;
-	// }
+	
 
 }
+
